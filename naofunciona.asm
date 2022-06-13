fdef7b6c5c8c9eebe68698b847f6ac8a875e92bb
Annagmo <85114312+Annagmo@users.noreply.github.com>
2022-06-13 19:33:29 -0300

    Update naofunciona.asm

--- naofunciona.asm
+++ naofunciona.asm
@@ -1,7 +1,6 @@
 .data
 
-bemVindo: 
- 	 	 	.asciiz "\t\t\t\t***INSTRUCÕES***\n"  
+                        bemVindo: .asciiz "\t\t\t\t***INSTRUCÕES***\n"  
 	        	instrucoes: .asciiz "           Você deve completar o tabuleiro em todo lugar que haja o número 0.\n   Será perguntado qual número você quer por em seu lugar, o índice da linha e o da coluna.\n             Lembre-se que os índices das linhas e colunas começam em 0.\n\t\t\t\t **Boa sorte**\n"
 	        	iniciaJogo: .asciiz "Digite 1 para jogar ou 0 para sair:\n"
 	        	nv_linha: .asciiz "|\n"
@@ -11,13 +10,12 @@ bemVindo:
 	        	acertou: .asciiz "Você acertou!  \\(°V°)/ \n"
 	        	nr: .asciiz "Qual número você deseja adicionar?\n"
 	        	linha: .asciiz "índice da linha:\n"
-        	 	col: .asciiz "índice da coluna:\n"
   
 jogo:       
-        	 	.byte     1, 0, 0, 0   
-        	 	.byte     0, 0, 0, 2  
-        	 	.byte     0, 3, 0, 0
-        	 	.byte     0, 0, 4, 0
+        	 	.byte     0, 2, 3, 4   
+        	 	.byte     3, 0, 1, 2  
+        	 	.byte     4, 3, 0, 1
+        	 	.byte     2, 1, 4, 0
         	           
 .text
         	 	li $v0,4 
@@ -160,168 +158,56 @@ chute:
 	         	syscall
          		move $a2,$v0 #movemos para guardar o índice da linha num registrador
          		
-	         	li $v0,4 #pergunta: "índice da coluna:\n"
-	         	la $a0,col 
-	         	syscall
-
-	         	li $v0,5 #lê o índice da coluna
-	         	syscall #execute the syscall
-	         	move $a3,$v0 #movemos para guardar o índice da coluna num registrador
-	         
+	     
 	         	move $a0,$s0
 	         	move $t5,$s0 # move o endereço de retorno da pilha que tá armazenando o tabuleiro para um registrador manipulável
 	         	# que não vai alterar o original
 	         	
-	         	
+	         	li $t0,0
+	         	li $t1,1
+	         	li $t2,2
+	         	li $t3,3
+	         	li $t4,4
 	         	#Checagem do chute:
-	         	jal checagem_chute
+	         	beq $t0, $a2, linha0
+	         	beq $t1, $a2, linha1
+	         	beq $t2, $a2, linha2
+	         	beq $t3, $a2, linha3
 	         	
-	         	# se o chute for correto:
-	         	beqz $v0,chute_correto #vai para o ramo de chute correto, pq a gente tem que mudar o tabuleiro
-	         	li $v0,4
-	         	#se for errado
-	         	la $a0,errou #só iprime já que não vai mudar o tabuleiro.
-         		syscall
-			
-         		move $a0,$s0 #move o endereço de retorno da pilha para o a0 para fazermos um novo chute 
-  
-               		jal chute # novo chute
-               		
+	   
+#elem 00           
+linha0:
+	beq $a1, $t1, chute_correto   # 1, 4, 2, 3 é a Diagonal principal
+	
+	j chute_errado
+	
+#elem 11	
+linha1:
+	beq $a1, $t4, chute_correto
+	
+	j chute_errado
+	
+#elem 22	
+linha2:
+	beq $a1, $t2, chute_correto
+	
+	j chute_errado 
+	
+#elem 33	
+linha3:
+	beq $a1, $t3, chute_correto
+	
+	j chute_errado  
+	
+
+	       		       			       		       		
 chute_correto:
 	         	li $v0,4 #imprime a mensagem de acerto:
 	         	la $a0,acertou # "Você acertou!  \\(°V°)/ \n"
 	         	syscall
 			
-	         	move $a0,$s0 #board address
-	         	jal FULL
-	         	beqz $v0,notfull
-	         	
-	         	# a prilha, para quando formos fazer uma nova pilha no ramo chute, 
-	         	# quando for ser guardada a linha que objeteve um acerto, ela já vai ser guardada atualizada
-	         	lw $s0, 0($sp) #restaura tudo para o que era
-	         	lw $s1, 4($sp) 
-	         	lw $s2, 8($sp) 
-	         	lw $ra, 12($sp)
-         		addi $sp, $sp, 16 # apaga a pilha
-	         	jr $ra # volta para o endereço de retorno que tava no início para criar uma pilha nova no mesmo endereço
-
-checagem_chute:
-	         	subi $sp,$sp,4 #make room in the stack
-	         	sw $ra,($sp) # salvar o endereço de retorno do ra
-	         	move $s0,$a0 ## salvar o endereço do tabuleiro atualizado com o chute
-	         	move $t9,$a1 #salva o chute
-         	
-	         	#chacagem de linha por linha pra ver se o chute entra
-	         	li $t0,4 #passar pelas 4 linhas
-	         	mul $t1,$a2,$t0 #Offset of the first cell in the row
-	         		         
-	         		         	
-	         		         		
-	         		         			
-	         		         				
-	         		         					
-	         		         						
-
-
-	
-check_row:
-	         	add $s1,$s0,$t1
-	         	lb $t2,($s1) # Value in the current cell
-	         	beq $t2,$t9,check_ret_fail #Number already present in row
-	         	addi $t1,$t1,1 #Increment the pointer to the current cell
-	         	subi $t0,$t0,1 # Decrement the counter
-	         	bnez $t0,check_row # Check the next cell in the row
-	         	
-	         	#Column check
-	         	move $t1,$a3 #Offset of the first cell in the column
-
-check_col:
-	         	add $s1,$s0,$t1
-	         	lb $t2,($s1) # Value of the current cell
-	         	beq $t2,$t9,check_ret_fail # Number already present in column
-	         	addi $t1,$t1,4 # Increment the pointer to the current cell
-	         	ble $t1,81,check_col # Check the next cell in the column
-	
-	         	# 3x3-Box check
-	         	div $t0,$a2,2 #$t0=row/3
-	         	mul $t0,$t0,16 #Offset of the row
-	         	div $t1,$a3,2 #$t3= col / 3
-	         	mul $t1,$t1,2 #Offset of the column
-	         	add $t1,$t0,$t1 # Offset of the first cell in the box
-	         	li  $t0, 2 # Set up the row counter
-	         	li  $t3, 2 # Set up the column counter
-    	
-check_box:
-	         	add $s1,$s0,$t1 
-	         	lb $t6,($s1) # Value of the current cell
-    	     	 	beq $t9,$t6,check_ret_fail #Number already present in column
-    	     	 	subi $t3,$t3,1 #Decrement the column counter
-    	     	 	beqz $t3,end_box_row # Check if end of current box row is reached
-    	     	 	addi $t1,$t1,1 #Increment the pointer to the current cell
-    	     	 	j check_box
-      
-end_box_row:
-	         	addi $t1,$t1,7 # Increment the pointer to the current cell
-	         	li $t3,2 #Reset column counter
-    	     	 	subi $t0,$t0,1 #Decrement the row counter
-    	     	 	bnez $t0,check_box # Check if end of box is reached
-    	     	 	li $t8,4
-    	     	 	mul $t7,$a2,$t8 #9*row_index
-    	     	 	add $t7,$a3,$t7 #9*row_index+column_index
-    	     	 	add $s1,$s0,$t7
-    	     	 	sb $t9,($s1) #store byte
-    	     	 	move $a0,$s0
-    	     	 	jal imprime_linha
-    	     	 	move $v0,$zero #Return code is 0 (success)
-    	     	 	j check_ret #Jump to the return instrucoes
-  
-check_ret_fail:
-    	     	 	li $t8,4
-    	     	 	mul $t7,$a2,$t8 #9*row_index
-    	     	 	add $t7,$a3,$t7 #9*row_index+column_index
-    	     	 	add $s1,$s0,$t7
-    	     	 	sb $t9,($s1) #store byte
-    	     	 	move $a0,$s0
-    	     	 	jal imprime_linha
-    	     	 	li $v0,1 #Return code is 1 (failure)
- 
-check_ret:
-	         	lw $ra,($sp) #Restore $ra register
-	         	addi $sp,$sp,4 #Clean up the stack frame
-	         	jr $ra #Return
-      
-FULL:
-	         	move $s0,$a0 #board address
-	        	move $s1,$zero #row counter
-	        	move $s2,$zero #column counter
-	        	move $t0,$s0 #save
-	        	li $t2,4
-	
-for1:
-	        	mul $t1, $s1, $t2
-  
-for2:
-	        	add $t3, $t1, $s2
-	        	add $s0, $s0, $t3
-	        	lb $t4,($s0)
-	        	beqz $t4, NOTFULL
-	        	addi $s2, $s2, 1
-	        	beq $s2, 4, end2
-	        	j for2
-  
-end2:
-	        	addi $s1, $s1, 1
-	        	beq $s1, 4, end1
-	        	move $s2, $zero #Reset column counter
-	        	j for1
-  
-end1:
-	        	li $v0, 1 #full
-	        	jr $ra
-  
-NOTFULL:
-	        	move $v0,$zero #Not Full
-	        	jr $ra	         		         								
-notfull:
-	         	move $a0,$s0 #board address
-	         	jal chute
+chute_errado:
+	         	li $v0,4
+	         	la $a0,errou
+	         	syscall	         	
+      		       	         	
